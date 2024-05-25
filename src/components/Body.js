ba2b57a519e4ce38bf4f5bbb47aba4762dffca71
Bikesh Yadav <coolbikesh07@gmail.com>
2024-05-25 21:04:55 +0545

    cors error fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -55,7 +55,7 @@ const Body = () => {
     }, []);
       
      async function fetchrestaurants(){
-     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING")
+     const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING")
 
      const json = await data.json();  
 
